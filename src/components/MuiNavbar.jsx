f6b9aa92783dfd69d2c4972a88e7de7ce99ff4d0
reddymanyam <reddappareddy18@gmail.com>
2024-11-11 16:48:46 +0530

    MuiNavbar

--- src/components/MuiNavbar.jsx
+++ src/components/MuiNavbar.jsx
@@ -1,11 +1,16 @@
-import { AppBar, Button, IconButton, Stack, Toolbar, Typography } from '@mui/material'
+import { AppBar, Button, IconButton, Menu, Stack, Toolbar, Typography } from '@mui/material'
 import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
 import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
 
-const MuiNavbar = () => {
+
+const MuiNavbar = ({setIsDrawerOpen}) => {
   return (
     <AppBar>
         <Toolbar>
+        <IconButton edge='start' onClick={()=>setIsDrawerOpen(true)}>
+            <MenuIcon />
+        </IconButton>
             <Stack direction="row" flexGrow={1}>
                 <IconButton size='large' aria-label='logo' >
                     <CatchingPokemonIcon />
@@ -19,6 +24,9 @@ const MuiNavbar = () => {
                 <Button variant='contained' color='secondary'>About</Button>
                 <Button variant='contained' color='secondary'>Logout</Button>
             </Stack>
+            <Menu>
+                
+            </Menu>
         </Toolbar>
     </AppBar>
   )
