f17c7365d40ae60a535efecd0ee70e52e540f0b0
reddymanyam <reddappareddy18@gmail.com>
2024-11-11 18:05:26 +0530

    MuiDialog

---
+++ src/components/MuiDialog.jsx
@@ -0,0 +1,27 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material'
+import React, { useState } from 'react'
+const MuiDialog = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <>
+            <Button onClick={()=>{setIsOpen(true)}} variant='contained' color='secondary' sx={{position:"absolute", bottom:"2%", right:"2%"}}>Open Dialog</Button>
+            <Dialog open={isOpen} onClose={()=>setIsOpen(false)}>
+                <DialogTitle>
+                    Submit the test?
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Are you sure you want to submit the test, you will not able to edit the test after submitting...
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={()=>setIsOpen(false)}>Cancel</Button>
+                    <Button onClick={()=>setIsOpen(false)}>Submit</Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    )
+}
+
+export default MuiDialog
