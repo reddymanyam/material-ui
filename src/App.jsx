a03f3cdd27e8920961545ff080dafbe1706e754e
reddymanyam <reddappareddy18@gmail.com>
2024-11-11 10:30:20 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Typographyui from './components/MuiTypography'
 import MuiButton from './components/MuiButton'
 import MuiTextfields from './components/MuiTextfields'
 import MuiSelect from './components/MuiSelect'
+import MuiCheckbox from './components/MuiCheckbox'
+import MuiBoxs from './components/MuiBoxs'
+import MuiGrids from './components/MuiGrids'
 
 const App = () => {
   return (
@@ -10,7 +13,10 @@ const App = () => {
     {/* <Typographyui /> */}
     {/* <MuiButton /> */}
     {/* <MuiTextfields /> */}
-    <MuiSelect />
+    {/* <MuiSelect /> */}
+    {/* <MuiCheckbox /> */}
+    <MuiBoxs />
+    {/* <MuiGrids /> */}
     </>
   )
 }
