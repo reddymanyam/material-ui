a0ebba12ac83900094bb0130f3453b9f48713302
reddymanyam <reddappareddy18@gmail.com>
2024-11-12 11:25:03 +0530

    MuiDialog

--- src/components/MuiDialog.jsx
+++ src/components/MuiDialog.jsx
@@ -1,13 +1,22 @@
-import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material'
+import { Paper, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material'
 import React, { useState } from 'react'
+import Draggable from 'react-draggable';
+
 const MuiDialog = () => {
     const [isOpen, setIsOpen] = useState(false);
 
+    const PaperComponent = (props) =>{
+                return(
+                    <Draggable  handle="#draggable-dialog-title"   cancel={'[class*="MuiDialogContent-root"]'}>
+                             <Paper {...props} />
+                    </Draggable>
+                )        
+    }
     return (
         <>
             <Button onClick={() => { setIsOpen(true) }} variant='contained' color='secondary' sx={{ position: "absolute", bottom: "2%", right: "2%" }}>Open Dialog</Button>
-            <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
-                <DialogTitle>
+            <Dialog open={isOpen} onClose={() => setIsOpen(false)} PaperComponent={PaperComponent}>
+                <DialogTitle style={{cursor:"move"}} id="draggable-dialog-title">
                     Submit the test?
                 </DialogTitle>
                 <DialogContent>
