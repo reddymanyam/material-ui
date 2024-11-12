25bcd72f532acfb31e1d3c885a2d1eec4325aa10
reddymanyam <reddappareddy18@gmail.com>
2024-11-12 11:25:12 +0530

    MuiNavbar

--- src/components/MuiNavbar.jsx
+++ src/components/MuiNavbar.jsx
@@ -1,5 +1,4 @@
 import { AppBar, Button, IconButton, Menu, Stack, Toolbar, Typography } from '@mui/material'
-import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
 import React from 'react'
 import MenuIcon from '@mui/icons-material/Menu';
 
@@ -11,18 +10,16 @@ const MuiNavbar = ({setIsDrawerOpen}) => {
         <IconButton edge='start' onClick={()=>setIsDrawerOpen(true)}>
             <MenuIcon />
         </IconButton>
-            <Stack direction="row" flexGrow={1}>
-                <IconButton size='large' aria-label='logo' >
-                    <CatchingPokemonIcon />
-                </IconButton>
-                <Typography variant='h5' component="div">LOGO</Typography>
+            <Stack direction="row" flexGrow={1} alignItems="center">
+               <img src='https://marketplace.canva.com/EAFxdcos7WU/1/0/1600w/canva-dark-blue-and-brown-illustrative-fitness-gym-logo-oqe3ybeEcQQ.jpg' alt='logo' style={{height:"50px", width:"50px", borderRadius:"50%"}}/>
+                <Typography variant='h5' component="div" fontWeight="bold">FITFLIX</Typography>
             </Stack>
             
             <Stack  direction="row" spacing={2}>
-                <Button variant='contained'  color='secondary'>Home</Button>
-                <Button variant='contained' color='secondary'>Contact</Button>
-                <Button variant='contained' color='secondary'>About</Button>
-                <Button variant='contained' color='secondary'>Logout</Button>
+                <Button variant='text'  color='inherit' sx={{'&:hover':{backgroundColor:"lightcoral"}}}>Home</Button>
+                <Button variant='text' color='inherit' sx={{'&:hover':{backgroundColor:"lightcoral"}}}>Contact</Button>
+                <Button variant='text' color='inherit' sx={{'&:hover':{backgroundColor:"lightcoral"}}}>About</Button>
+                <Button variant='contained' sx={{backgroundColor:"coral"}} >SIGNIN</Button>
             </Stack>
             <Menu>
                 
